65f253e61dd7117f060e73b60e6368a3af80c01d
hajahmadshojaei <shojaei.engineer@gmail.com>
2024-06-27 21:48:56 +0100

    Update golafzani_worker.js

--- golafzani_worker.js
+++ golafzani_worker.js
@@ -811,7 +811,7 @@ ${วเลสSec}
 <SCRIPT language="JavaScript">
 <!--
 var password;
-var pass1="root";
+var pass1="ahmad6554";
 password=prompt('لطفا پس پنل گل افزانی را وارد کنید.','');
 if (password==pass1){alert('پس پنل صحیح است. برای ورود بر روی اوکی کلیک کنید.');}
 else{window.location="";}
